23bbc1529f406f8c333f1230b4b2281450f878f7
Niklas0010 <nlyberg@gmail.com>
2021-08-10 13:18:53 +0200

    .

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm  } from 'react-hook-form'
 
 export function Login() {
 
-  const { register, handleSubmit, errors} = useForm()
+  const {
+  register,
+  handleSubmit,
+  formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <b>Log ind</b>
         <label>Username</label>
-        <input name="username" {...register('username', { required: true })}></input>
+        <input name="username" {...register("username", { required: true })}></input>
+            {errors.username && <span>Udfyld username</span>}
 
         <label>Password</label>
         <input name="password" type="password" {...register('password', { required: true })}></input>
-      
+            {errors.password && <span>Udfuld password</span>}
+
         <button>LOG IND</button>
     </form>
   )
